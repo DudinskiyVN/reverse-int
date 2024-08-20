7d80a69c59d1a916d139c6cf512d7719ec8f7aa0
Dudinskiyvn <Dudinskiyvn@gmail.com>
2024-08-20 23:52:17 +0300

    docs: modified index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+if (n < 0) n = -n;    
+let str_n = String(n);
+let str_n_rev = '';    
+for (let i = 0; i < str_n.length; i++)  str_n_rev = str_n_rev + str_n[str_n.length - i-1];
+return Number(str_n_rev); 
 }
